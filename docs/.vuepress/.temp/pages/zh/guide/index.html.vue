07c1530a35a95fe3a82b995f2ba2ac897b2eeffe
CoderSerio <2779066456@qq.com>
2023-10-16 01:32:15 +0800

    feat: 🪳  临时commit

--- docs/.vuepress/.temp/pages/zh/guide/index.html.vue
+++ docs/.vuepress/.temp/pages/zh/guide/index.html.vue
@@ -1,5 +1,9 @@
 <template><div><h1 id="指南" tabindex="-1"><a class="header-anchor" href="#指南" aria-hidden="true">#</a> 指南</h1>
 <h2 id="lec3d是什么" tabindex="-1"><a class="header-anchor" href="#lec3d是什么" aria-hidden="true">#</a> lec3d是什么</h2>
+<h3 id="理念" tabindex="-1"><a class="header-anchor" href="#理念" aria-hidden="true">#</a> 理念</h3>
+<h3 id="快速开始" tabindex="-1"><a class="header-anchor" href="#快速开始" aria-hidden="true">#</a> 快速开始</h3>
+<h3 id="贡献和维护" tabindex="-1"><a class="header-anchor" href="#贡献和维护" aria-hidden="true">#</a> 贡献和维护</h3>
+<h2 id="api" tabindex="-1"><a class="header-anchor" href="#api" aria-hidden="true">#</a> API</h2>
 </div></template>
 
 
